fd8656191b4ba591b8ea4e7c1f8e9cf0f3431490
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-26 12:39:41 +0900

    java1復習_1

--- javalesson1.java
+++ javalesson1.java
@@ -1,3 +1,9 @@
 public class javalesson1 {
-  
+  public static void main(String[]ages){
+    int number = 8;
+    int number2 = 4;
+    int number3 = number/number2;
+    System.out.println(number3);
+    System.out.println(number3*number2);
+  }
 }
