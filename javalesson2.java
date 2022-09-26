cc6a876610c2f9f7465fc151c7ea940499871529
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-26 12:44:45 +0900

    java復習_2

---
+++ javalesson2.java
@@ -0,0 +1,10 @@
+public class javalesson2 {
+ public static void main(String[]ages){
+  int number = 7;
+  if(number>5 && number<20){
+    System.out.println("ok");
+  }else{
+    System.out.println("ng");
+  }
+ }
+}
