1fe494df687822e861aa3062bba5bbcd66bdf994
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-26 14:04:33 +0900

    java復習_8

---
+++ javalesson10.java
@@ -0,0 +1,13 @@
+public class javalesson10 {
+  public static void main(String[]ages){
+    int num = 2;
+    for(int i=1; i <= num; i++){
+      if(num==0){
+        System.out.println("");
+      }else{
+      System.out.println("*");
+      }
+    }
+    }
+  }
+
