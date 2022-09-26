dc6b46228ea7ab0af2d0d82110a51d7c34dde78c
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-26 13:45:27 +0900

    java復習_7

---
+++ javalesson9.java
@@ -0,0 +1,11 @@
+public class javalesson9 {
+  public static void main(String[]ages){
+    int num1 = 4;
+    int num2 = 2;
+    int num3 = 54;
+    int num4 = 5;
+    int num5 = 6;
+    int numall = num1+num2+num3+num4+num5;
+    System.out.println(numall);
+  }
+}
