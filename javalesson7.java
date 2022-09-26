fdd9b94bd04ff2d784f49338339c90e23c53ef21
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-26 13:27:57 +0900

    java復習_5

---
+++ javalesson7.java
@@ -0,0 +1,11 @@
+public class javalesson7 {
+  public static void main(String[]ages){
+    int number = 5;
+    for(int i=0; i<=number; i++){
+      System.out.println(i);
+    }
+    if(number == 0){
+      System.out.println(0);
+    }
+  }
+}
