5f6f67220e2eed283636ff8d2518e809cfe4ba91
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-26 14:36:29 +0900

    java復習_14

---
+++ javalesson16.java
@@ -0,0 +1,8 @@
+public class javalesson16 {
+  public static void main(String[]ages){
+    int[] num = {3, 7, 0, 8, 4, 1, 9, 6, 5, 2};
+    int sum = 3;
+    int sum2 = 4;
+    System.out.println(num[sum]*num[sum2]);
+  }
+}
