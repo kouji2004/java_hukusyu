f1ef6bae29eb8611ec143ed3c045c96558b9941d
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-26 13:41:05 +0900

    java復習_6

---
+++ javalesson8.java
@@ -0,0 +1,9 @@
+public class javalesson8 {
+  public static void main(String[]ages) {
+    int num1 = 2;
+    for (int i = 1; i <= 10; i++) {
+        num1 = num1 * i;
+    }
+    System.out.println(num1);
+}
+}
