2ee777c0e4ee5a6090a2278e318f4c20b894a224
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-26 14:42:16 +0900

    java復習_15

--- javalesson17.java
+++ javalesson17.java
@@ -1,3 +1,8 @@
 public class javalesson17 {
-  
+  public static void main(String[]ages){
+    int[] num = {3, 7, 0, 8, 4, 1, 9, 6, 5, 2};
+    int number = 3;
+    int number2 = num[number];
+    System.out.println(number2);
+  }
 }
