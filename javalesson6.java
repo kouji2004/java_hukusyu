4af0a638308541cc04ce490fb97a23532cb25a23
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-26 13:20:48 +0900

    java復習_4(switch)

---
+++ javalesson6.java
@@ -0,0 +1,18 @@
+public class javalesson6 {
+  public static void main(String[]ages){
+    int number = 2;
+    switch(number){
+      case 1:
+        System.out.println("one");
+        break;
+      case 2:
+        System.out.println("twoo");
+        break;
+      case 3:
+        System.out.println("three");
+        break;
+      default:
+      System.out.println("ohh");
+    }
+  }
+}
