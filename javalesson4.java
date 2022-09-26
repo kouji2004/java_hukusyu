0723751d8bac5be3c8c02d6970b3cdd070057b7c
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-26 12:54:20 +0900

    java復習_3

---
+++ javalesson4.java
@@ -0,0 +1,10 @@
+public class javalesson4 {
+  public static void main(String[]ages){
+   int number = -10;
+   if(number <= -10 || number >= 10) {
+    System.out.println("ng");
+   }else{
+    System.out.println("ok");
+   }
+  }
+}
