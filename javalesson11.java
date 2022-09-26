4615a5c10357761b350ce2c65af919f996601f16
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-26 14:13:19 +0900

    java復習9

---
+++ javalesson11.java
@@ -0,0 +1,12 @@
+public class javalesson11 {
+  public static void main(String[]ages){
+    int num = 6;
+  for(int i=1; i<=num; i++){
+    if(i%5==0){
+      System.out.println("\t");
+    }else{
+      System.out.println("*");
+    }
+  }
+  }
+}
