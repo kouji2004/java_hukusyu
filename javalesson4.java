595bbd380035d1f071c6ce8579c640535c73b7e5
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-26 13:10:20 +0900

    java復習_4

--- javalesson4.java
+++ javalesson4.java
@@ -1,10 +1,10 @@
 public class javalesson4 {
   public static void main(String[]ages){
-   int number = -10;
-   if(number <= -10 || number >= 10) {
-    System.out.println("ng");
-   }else{
-    System.out.println("ok");
-   }
-  }
+ int number = -10;
+ if((number>=-10&& number<0 || (number>=10))){
+  System.out.println("ok");
+ }else{
+  System.out.println("ng");
+ }
+}
 }
---
+++ javalesson5.java
@@ -0,0 +1,12 @@
+public class javalesson5 {
+  public static void main(String[]ages){
+    int number = -12;
+    if(number <= -10){
+      System.out.println("range");
+    }else if(number>=1 && number<0 || number>=-10 && number<0){
+      System.out.println("range2");
+    }else if(number >= 0){
+      System.out.println("range3");
+    }
+  }
+}
