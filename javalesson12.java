bade1739f16189df7fd6faf1f17c4e03851814be
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-26 14:15:57 +0900

    java復習_10

---
+++ javalesson12.java
@@ -0,0 +1,11 @@
+public class javalesson12 {
+  public static void main(String[]ages){
+    for(int i=1; i<=20; i++){
+      if(i%5==0){
+        System.out.println("ber");
+      }else{
+        System.out.println(i);
+      }
+    }
+  }
+}
