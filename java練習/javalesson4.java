a577daf90d6297efc35d89167870472662b43959
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-28 13:16:10 +0900

    java.time.LocalDateで日付けの表示

---
+++ java練習/javalesson4.java
@@ -0,0 +1,10 @@
+package java練習;
+
+import java.time.LocalDate;
+
+public class javalesson4 {
+  public static void main(String[]ages){
+   java.time.LocalDate Id= java.time.LocalDate.now();
+   System.out.println(Id);
+  }
+}
