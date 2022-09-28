daa0db1942c30dcbec17c91abfadd51b3573db67
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-28 13:07:33 +0900

    今日の日付けを表示

---
+++ java練習/javalesson3.java
@@ -0,0 +1,10 @@
+package java練習;
+
+import java.time.LocalDate;
+
+public class javalesson3 {
+  public static void main(String[]ages){
+  LocalDate Id = LocalDate.now();
+  System.out.println(Id);
+  }
+}
