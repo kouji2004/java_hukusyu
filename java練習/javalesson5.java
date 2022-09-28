f40af3d184c5861d1e635f30a53a62d7b08e7720
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-28 13:34:21 +0900

    BufferedReader

---
+++ java練習/javalesson5.java
@@ -0,0 +1,11 @@
+package java練習;
+
+public class javalesson5 {
+  
+}
+
+
+//BufferedReaderとは
+//BufferedReaderクラスとは、テキストファイルを読み込むためのクラスです。
+//BufferedReaderクラスでは、テキストファイルを1行ずつ読み込むreadlineメソッドが用意されています。
+//テキストファイルを読み込むクラスにはFileReaderクラスもありますが、こちらはテキストを1文字ずつ読み込むため効率が悪い場合があります。
