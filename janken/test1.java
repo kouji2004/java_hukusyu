a5162e8152b852ac222aac24f6d3ae18e8040339
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-01 20:40:18 +0900

    java　中級編　アルゴリズム

--- janken/test1.java
+++ janken/test1.java
@@ -2,10 +2,8 @@ package janken;
 
 public class test1 {
   public static void main(String[]ages){
-    int inputValue = 1;
-
     double randNum = Math.random();
-    int userNum = 1: //1:グー 2:チョキ 3:パー
+    int userNum = 1; //1:グー 2:チョキ 3:パー
 
     //機械の手を乱数を使って判定していく
     int machineNum = 0;
@@ -17,18 +15,19 @@ public class test1 {
     }else{
       machineNum = 3;
     }
+  // 勝敗の判定
+  if(userNum==machineNum)
 
-  }
-  //勝敗の判定
-  if(userNum == machineNum ){
+  {
     System.out.println("あいこ");
-  }else if((userNum==1 && machineNum == 2)
-         ||(userNum==2 && machineNum == 3)
-         ||(userNum==3 && machineNum == 1)
-        ){
-          System.out.println("勝ち");
-  }else{
+  }else if((userNum==1&&machineNum==2)||(userNum==2&&machineNum==3)||(userNum==3&&machineNum==1))
+  {
+    System.out.println("勝ち");
+  }else
+  {
     System.out.println("負け");
+  }
+  System.out.println(machineNum+"出した手");
   }
 
 }
