8559846d44481bda809c1f0bba8c5aeae1ad9545
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-16 21:38:36 +0900

    奇数で表示

--- java復習その１/fukusyu.java
+++ java復習その１/fukusyu.java
@@ -1,7 +1,7 @@
 
 public class fukusyu {
   public static void main(String[] ages) {
-    for (int i = 0; i <= 10; i++) {
+    for (int i = 0; i <= 100; i++) {
       if (i % 2 == 0) {
         System.out.println(i + 1);
       }
