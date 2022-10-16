9178273f61fea8581246be827a5a713d5ce3135c
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-16 21:37:42 +0900

    偶数で表示

--- java復習その１/fukusyu2.java
+++ java復習その１/fukusyu2.java
@@ -1,6 +1,6 @@
 public class fukusyu {
   public static void main(String[] ages) {
-    for (int i = 0; i <= 10; i++) {
+    for (int i = 0; i <= 100; i++) {
       if (i % 2 == 0) {
         System.out.println(i);
       }
