53b224f23684c08f1f9d51cb6bbd4e34451f4212
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-18 21:12:38 +0900

    日付け

---
+++ java練習/java13.java
@@ -0,0 +1,13 @@
+package java練習;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class java13 {
+  public static void main(String[] args) {
+    Calendar cal = Calendar.getInstance();
+    Date date = cal.getTime();
+    System.out.println(date);
+  }
+
+}
